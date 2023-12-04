ad68aea300ebe14b16e7c7e8deb862e7cf95989d
sachindsilva16 <sachindsilvad@gmail.com>
2023-12-04 11:40:36 +0530

    Update readme

--- TEST_02/04.py
+++ TEST_02/04.py
@@ -10,4 +10,43 @@ def compress_point(point):
     return hex(point.x)+hex(point.y % 2)[2:]
 
 def getEnKey(publicKey):
-    
+    ciPrivateKey = secrets.randbelow(curve.field.n)
+    ciPublicKey = ciPrivateKey * curve.g
+    enKey = ciPrivateKey * publicKey
+    return (enKey,ciPublicKey)
+
+
+senderPrivateKey = secrets.randbelow(curve.field.n)
+senderPublicKey = senderPrivateKey *curve.g
+
+print("Sender's private key : ",hex(senderPrivateKey))
+print("Sender's public key : ",compress_point(senderPublicKey))
+
+print("\n")
+
+
+(senderEncryptionKey,ciPublicKeyOfSender) = getEnKey(senderPublicKey)
+
+print("Sender's encryption key : ",compress_point(senderEncryptionKey))
+print("Sender's ciphertext public key : ",compress_point(ciPublicKeyOfSender))
+print("\n")
+
+
+
+receiverPrivateKey = secrets.randbelow(curve.field.n)
+receiverPublicKey = receiverPrivateKey * curve.g
+
+print("Receiver's private key : ",hex(receiverPrivateKey))
+print("Receiver's public key : ",compress_point(receiverPublicKey))
+
+print("\n")
+
+(receiverEncryptionKey,ciPublicKeyOfReceiver) = getEnKey(receiverPublicKey)
+
+print("Receiver's encryption key : ",compress_point(receiverEncryptionKey))
+print("Receiver's ciphertext public key : ",compress_point(ciPublicKeyOfReceiver))
+
+print("\n")
+
+
+
